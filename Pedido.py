4a761e497bd2b9e6da4af59831a8ad2e31059f9d
Abigail400 <163369047+Abigail400@users.noreply.github.com>
2026-01-17 23:27:12 -0800

    Update Pedido.py

--- Pedido.py
+++ Pedido.py
@@ -1,12 +1,12 @@
 class Pedido:
-    def __init__(self, PedidoID=0, NombrePropie="", platilloID=0, HoraEntrega=""):
+    def __init__(self, PedidoID=0, NombrePropie="", platilloID=0):
         self.__pedidoID=PedidoID
         self.__NombrePropietario=NombrePropie
         self.__platilloID= platilloID
         self.__HoraEntrega=HoraEntrega
         
     def __str__(self):  
-        return f"{self.__pedidoID}{self.__NombrePropietario}{self.__platilloID}{self.__HoraEntrega}"
+        return f"{self.__pedidoID}{self.__NombrePropietario}{self.__platilloID}"
     
     def get_PedidoID(self):
         return self.__pedidoID
@@ -23,7 +23,5 @@ class Pedido:
     def set_platillo(self, platilloID):
         self.__platilloID
     
-    def get_HoraEntrega(self):
-        return self.__HoraEntrega
-    def set_HoraEntrega(self, HoraEntrega):
-        self.__HoraEntrega=HoraEntrega
+ 
+   
